1dc5b53f1066583572bc5a40d32f6190de47a511
Pratha B <prathabodas@gmail.com>
2017-04-08 20:13:22 -0700

    Made Name Activity

---
+++ app/src/main/java/com/example/prathabodas/cupertinoconnect/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.prathabodas.cupertinoconnect;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+/**
+ * Created by prathabodas on 4/8/17.
+ */
+
+public class MainActivity extends Activity {
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+
+        final Button login = (Button)findViewById(R.id.login);
+       // final Button signUp = (Button) findViewById(R.id.signUp);
+    }
+}
